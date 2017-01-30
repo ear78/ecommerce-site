91cb2a6bb2adc2cb1e7bf2b1a609c7d8fccb42e0
elliot richardson <ear78@aol.com>
2017-01-29 23:13:13 -0600

    update json files and input for search

--- public/jacketComponent/jackets.component.js
+++ public/jacketComponent/jackets.component.js
@@ -2,6 +2,7 @@ angular.module('jackets')
     .component('jackets', {
         templateUrl: 'jacketComponent/jackets.component.html',
         controller: function JacketsController($http){
+             
             var self = this;
             $http.get('jackets/jacketList.json').then(function(response){
                 self.jacketList = response.data;
