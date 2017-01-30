e6e1428c531b3222be85ab978c5f652aa0115b69
elliot richardson <ear78@aol.com>
2017-01-30 00:40:05 -0600

    worked on filters for sorting list

--- public/jacketComponent/jackets.component.js
+++ public/jacketComponent/jackets.component.js
@@ -2,7 +2,7 @@ angular.module('jackets')
     .component('jackets', {
         templateUrl: 'jacketComponent/jackets.component.html',
         controller: function JacketsController($http){
-             
+            this.order = "jacket1";
             var self = this;
             $http.get('jackets/jacketList.json').then(function(response){
                 self.jacketList = response.data;
