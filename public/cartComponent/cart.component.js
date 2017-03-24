bda3b6b98e39ac328a49a48cb1365799cf0c847e
elliot richardson <ear78@aol.com>
2017-03-24 15:29:32 -0500

    added new json files for database and added small and large images

--- public/cartComponent/cart.component.js
+++ public/cartComponent/cart.component.js
@@ -1,4 +1,19 @@
 angular.module('cart')
     .component('cart', {
-        templateUrl: 'cartComponent/cart.component.html'
+        templateUrl: 'cartComponent/cart.component.html',
+        controller: function cartCtrl($http, $routeParams){
+            this.quantity = 0;
+            this.subtotal = 0;
+            this.tax = 0;
+            this.total = 0;
+
+            this.addToCart = function($routeParams.id){
+                console.log($routeParams.id);
+            }
+            //GETTING JACKET LIST FROM JSON FILE AND SERVICE
+            $http.get('jackets/jacketList.json').then(function(response){
+                this.jacketList = response.data;
+            })
+
+        }
     })
