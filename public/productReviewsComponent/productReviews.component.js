de1445d834fa3c0820ef8447c0c670690de3c262
elliot richardson <ear78@aol.com>
2017-03-27 23:13:08 -0500

    worked on creating the review functionality between components

--- public/productReviewsComponent/productReviews.component.js
+++ public/productReviewsComponent/productReviews.component.js
@@ -4,6 +4,14 @@ angular.module('eApp')
         controller:  function($http, $routeParams){
             var self = this;
 
+            self.review = {};
+
+            self.addReview = function(review){
+                console.log('submitted');
+                this.review = {};
+            }
+
+            // JSON DATA COMING IN FROM PRODUCT JSON FILES
             $http.get('jackets/' + $routeParams.jacketId + '.json').then(function(response){
                 console.log(response.data);
                 self.jackets = response.data[0];
