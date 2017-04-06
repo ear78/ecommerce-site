197a344c0d7257a9806cf9c13a09eb9b2019c659
elliot richardson <ear78@aol.com>
2017-04-06 14:16:06 -0500

    added closing functionality for nav menu on mouseleave and link click, added css as well

--- public/directives/menuClick/menuClick.directive.js
+++ public/directives/menuClick/menuClick.directive.js
@@ -3,8 +3,19 @@ angular.module('eApp')
         return {
             restrict: 'A',
             link: function(scope,element,attrs){
+                var $dropContainer = $('.drop-container');
+
                 element.on('click', function(){
                     $('.drop-container').slideToggle('fast');
+                })
+
+                //CLOSE MENU IF LINK IS CLICKED
+                $dropContainer.find('a').on('click', function(){
+                    $dropContainer.slideUp('fast');
+                })
+
+                $dropContainer.on('mouseleave', function(){
+                    $dropContainer.slideUp('fast');
                 })
             }
         }
