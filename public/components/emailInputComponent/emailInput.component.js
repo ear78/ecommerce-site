de0c6f1d164a79023df510749c047f7febfa8324
elliot richardson <ear78@aol.com>
2017-04-06 22:37:18 -0500

    added functionality for email subscription

--- public/components/emailInputComponent/emailInput.component.js
+++ public/components/emailInputComponent/emailInput.component.js
@@ -1,4 +1,15 @@
 angular.module('eApp')
     .component('emailInput', {
-        templateUrl: './components/emailInputComponent/emailInput.template.html'
+        templateUrl: './components/emailInputComponent/emailInput.template.html',
+        controller: function(){
+            var self = this;
+
+            self.emailInfo = '';
+
+            self.emailInput = function(emailInfo) {
+                console.log(emailInfo);
+                alert('Thanks for signing up ' + emailInfo);
+                self.emailInfo = '';
+            }
+        }
     })
