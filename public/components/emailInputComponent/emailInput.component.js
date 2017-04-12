3dea0fd8e87ff802e259182c465f11bd468dcbcb
elliot richardson <ear78@aol.com>
2017-04-12 17:32:21 -0500

    added closing animations functionality to modal thank you

--- public/components/emailInputComponent/emailInput.component.js
+++ public/components/emailInputComponent/emailInput.component.js
@@ -1,19 +1,31 @@
 angular.module('eApp')
     .component('emailInput', {
         templateUrl: './components/emailInputComponent/emailInput.template.html',
-        controller: function(){
+        controller: function($timeout){
             var self = this;
 
+            //animation trigger
+            self.isActive = false;
+
+            //ng-hide trigger for component
             self.isSubmitted = true;
+
+            //reset email input box
             self.emailInfo = '';
 
             self.emailInput = function(emailInfo) {
                 self.emailInfo = '';
                 self.isSubmitted = false;
+                self.isActive = !self.isActive;
             }
 
             self.closeModal = function(){
-                self.isSubmitted = true;
+                self.isActive = false;
+                $timeout(function () {
+                    self.isSubmitted = true;
+                }, 1000);
+
+
             }
         }
     })
