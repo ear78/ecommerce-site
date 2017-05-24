a21193f092ed22455d86c54f60e9eedfda01e6ed
elliot richardson <ear78@aol.com>
2017-05-23 21:25:07 -0500

    added and refactored title component for mulitple pages

--- public/components/titleComponent/title.component.js
+++ public/components/titleComponent/title.component.js
@@ -1,4 +1,10 @@
 angular.module('eApp')
     .component('titleComponent', {
-        templateUrl: "./components/titleComponent/title.component.html"
+        templateUrl: "./components/titleComponent/title.component.html",
+        controller: function($route, $location){
+            var self = this;
+            this.$route = $route;
+
+            
+        }
     })
